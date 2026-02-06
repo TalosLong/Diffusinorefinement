6697462589b92a70924048f953d0fbfdd24e46f1
TalosLong <87523305+TalosLong@users.noreply.github.com>
2026-02-06 16:55:34 +0800

    Revert "Implement medical image segmentation system with diffusion-based boundary refinement"

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Models module for medical image segmentation
-"""
-
-from .cpunet import CPUNet
-from .diffusion import DiffusionRefinement, GaussianDiffusion
-
-__all__ = ["CPUNet", "DiffusionRefinement", "GaussianDiffusion"]
